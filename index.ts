00eb87aa89574ca1b13100ba521261eddf3a3816
Neel <neelusenthil1996@gmail.com>
2021-03-22 19:42:02 +0530

    remove unwanted json file variable

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import path from "path";
 import { CheckWithExtension } from "./lib/CheckWithExtension";
 import { CheckWithFile } from "./lib/CheckWithFile";
 import { CheckWithFileName } from "./lib/CheckWithFileName";
@@ -8,17 +7,10 @@ import { LangData } from "./lib/interface/LangDataInterface";
 import { LanguageType } from "./lib/types/LanguageType";
 import { DataFileReader } from "./utils/readFromDataFile";
 
-var dataFilePath: string = path.join(
-  __dirname,
-  "..",
-  "data/linguistDataSet.json"
-);
-
 var dataFileContent: LangData[] = [];
-
 export class LangLine {
   constructor() {
-    dataFileContent = new DataFileReader(dataFilePath).readFromFile();
+    dataFileContent = new DataFileReader().readFromFile();
   }
 
   /**
